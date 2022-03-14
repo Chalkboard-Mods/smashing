3ba84f9bf9b5bf033facf4cbc5756e974e24a139
Mana Kampman <manastar04@gmail.com>
2022-03-14 12:13:33 -0700

    Loot tables and tweaks

--- common/src/main/java/chalkboardmods/smashing/common/block/PotBlock.java
+++ common/src/main/java/chalkboardmods/smashing/common/block/PotBlock.java
@@ -2,16 +2,19 @@ package chalkboardmods.smashing.common.block;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.entity.item.FallingBlockEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.Projectile;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.FallingBlock;
+import net.minecraft.world.level.block.OreBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.BooleanOp;
@@ -37,6 +40,16 @@ public class PotBlock extends FallingBlock {
     }
 
     @Override
+    protected void popExperience(ServerLevel level, BlockPos pos, int amount) {
+        Random random = new Random();
+        int i = 0;
+        if (random.nextBoolean()) {
+            i = 1;
+        }
+        super.popExperience(level, pos, i);
+    }
+
+    @Override
     public void onLand(Level level, BlockPos pos, BlockState blockState, BlockState blockState2, FallingBlockEntity fallingBlock) {
         Random random = new Random();
 
@@ -46,6 +59,11 @@ public class PotBlock extends FallingBlock {
             level.addAlwaysVisibleParticle(ParticleTypes.CAMPFIRE_COSY_SMOKE, true, (double)pos.getX() + 0.5D + random.nextDouble() / 3.0D * (double)(random.nextBoolean() ? 1 : -1), (double)pos.getY() + random.nextDouble() + random.nextDouble(), (double)pos.getZ() + 0.5D + random.nextDouble() / 3.0D * (double)(random.nextBoolean() ? 1 : -1), 0.0D, 0.035D, 0.0D);
             level.playSound((Player) null, pos, SoundEvents.GLASS_BREAK, SoundSource.BLOCKS, 1.0f, 1.0f);
         }
+    }
+
+    @Override
+    public void spawnAfterBreak(BlockState state, ServerLevel level, BlockPos pos, ItemStack stack) {
+        super.spawnAfterBreak(state, level, pos, stack);
     }
 
     public static final VoxelShape SHAPE = Shapes.join(Shapes.join(Block.box(4, 7, 4, 12, 9, 12), Block.box(5, 6, 5, 11, 7, 11), BooleanOp.OR), Block.box(4, 0, 4, 12, 6, 12), BooleanOp.OR);
